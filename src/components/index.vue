b6863a80db8a7434f62049ef697c4ea813bf4aa4
Nurbol <nurbolmaden@gmail.com>
2020-02-27 19:43:39 +0600

    changes

--- src/components/index.vue
+++ src/components/index.vue
@@ -89,7 +89,7 @@
                         <p>© 2020 Частная школа «Akeac»</p>
                         <!-- <i class="fab fa-instagram"></i> -->
                         <!-- https://www.instagram.com/encode.kz/ -->
-                        <a href="#" target="_blank">Сайт разработан  в &nbsp encode.kz</a>
+                        <a href="#" target="_blank">Сайт разработан  в encode.kz</a>
                     </div>
                 </div>
     </div>
